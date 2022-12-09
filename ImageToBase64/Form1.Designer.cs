e6728f7f17dd061d45be837c796458cc16214352
haolaigou <haolaigou@DESKTOP-C0QJ13M>
2022-12-09 22:06:16 +0800

    添加项目文件。

---
+++ ImageToBase64/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace ImageToBase64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtBase64 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.picImage = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.toImageBtn = new System.Windows.Forms.Button();
+            this.toBase64Btn = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtBase64);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(399, 450);
+            this.panel2.TabIndex = 0;
+            // 
+            // txtBase64
+            // 
+            this.txtBase64.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtBase64.Location = new System.Drawing.Point(0, 12);
+            this.txtBase64.Multiline = true;
+            this.txtBase64.Name = "txtBase64";
+            this.txtBase64.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtBase64.Size = new System.Drawing.Size(399, 438);
+            this.txtBase64.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Base64 String";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(397, 450);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.picImage);
+            this.panel5.Controls.Add(this.label2);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(0, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(397, 400);
+            this.panel5.TabIndex = 1;
+            // 
+            // picImage
+            // 
+            this.picImage.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picImage.Location = new System.Drawing.Point(0, 12);
+            this.picImage.Name = "picImage";
+            this.picImage.Size = new System.Drawing.Size(397, 388);
+            this.picImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picImage.TabIndex = 0;
+            this.picImage.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Picture";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.openBtn);
+            this.panel4.Controls.Add(this.toImageBtn);
+            this.panel4.Controls.Add(this.toBase64Btn);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 400);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(397, 50);
+            this.panel4.TabIndex = 0;
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(23, 7);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(61, 35);
+            this.openBtn.TabIndex = 2;
+            this.openBtn.Text = "Open";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // toImageBtn
+            // 
+            this.toImageBtn.Location = new System.Drawing.Point(257, 9);
+            this.toImageBtn.Name = "toImageBtn";
+            this.toImageBtn.Size = new System.Drawing.Size(120, 35);
+            this.toImageBtn.TabIndex = 1;
+            this.toImageBtn.Text = "Base64 to Image";
+            this.toImageBtn.UseVisualStyleBackColor = true;
+            this.toImageBtn.Click += new System.EventHandler(this.toImageBtn_Click);
+            // 
+            // toBase64Btn
+            // 
+            this.toBase64Btn.Location = new System.Drawing.Point(126, 9);
+            this.toBase64Btn.Name = "toBase64Btn";
+            this.toBase64Btn.Size = new System.Drawing.Size(120, 35);
+            this.toBase64Btn.TabIndex = 0;
+            this.toBase64Btn.Text = "Image to Base64";
+            this.toBase64Btn.UseVisualStyleBackColor = true;
+            this.toBase64Btn.Click += new System.EventHandler(this.toBase64Btn_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel3);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 397;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.PictureBox picImage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button toImageBtn;
+        private System.Windows.Forms.Button toBase64Btn;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtBase64;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button openBtn;
+    }
+}
+
---
+++ ImageToBase64/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageToBase64
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+
+
+
+
+
+        public string ConvertImageToBase64(Image file)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                file.Save(memoryStream, file.RawFormat);
+                byte[] imageBytes = memoryStream.ToArray();
+                return Convert.ToBase64String(imageBytes);
+            }
+        }
+
+
+        public Image ConvertBase64ToImage(string base64String)
+        {
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            using (MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+            {
+                ms.Write(imageBytes, 0, imageBytes.Length);
+                return Image.FromStream(ms, true);
+            }
+        }
+
+
+
+        private void toBase64Btn_Click(object sender, EventArgs e)
+        {
+            if (picImage.Image == null)
+                return;
+
+            using (Image image = picImage.Image.Clone() as Image)
+            {
+                txtBase64.Text = ConvertImageToBase64(image);
+            }
+        }
+
+        private void toImageBtn_Click(object sender, EventArgs e)
+        {
+            picImage.Image = ConvertBase64ToImage(txtBase64.Text);
+        }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Picture|*.gif;*.jpg;*.jpeg;*.bmp;*.jfif;*.png;|All|*.*";
+            var result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                var filename = openFileDialog.FileName;
+
+                if (File.Exists(filename))
+                    picImage.ImageLocation = filename;
+            }
+        }
+
+
+    }
+}
